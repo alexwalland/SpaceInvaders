4c7a01863145f640d20cbea9afa663fe50c228da
alexwalland <beastdrogba@gmail.com>
2021-11-03 10:37:02 +0000

    Add files via upload

---
+++ Assets/Scripts/GlobalVariables.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables : MonoBehaviour
+{
+    public static float speed = 30;
+    public static float direction = 1;
+    public static int lives = 3;
+    public static float score = 0;
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject bullet;
+
+    float shootDelay = 0.5f;
+    float timer = 1;
+
+    void FixedUpdate ()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move, 0) * GlobalVariables.speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetButtonDown("shoot") && timer > shootDelay)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            timer = 0;
+        }
+    }
+}
